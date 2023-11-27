3311bc236b40308ccd9ac1940762c1b904936619
Muhammed Elşami <muhammed.elsami@detaysoft.com>
2023-11-27 20:10:26 +0300

    Fix Lottie animations

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import Lottie from 'lottie-react';
+import animationData from "@/public/animation.json";
 
 function HomePage() {
     return (
         <>
         <section id="tabView1" className="tab active-tab">
-            <Player
+            <Lottie 
+                animationData={animationData}
+                style={{width: '40vh' }}
                 autoplay
                 loop
-                src={'https://lottie.host/b4d2c9f3-6bac-4682-8cf2-7704287fd2e5/9XaHId2Lry.json'}
-                style={{ height: '100%', width: '40vh' }}
-                direction={1}
-                speed={1}
-                />
+             />
             <div>
                 <p id="about-app-txt">
                     The Firebase Server Key is a tool that provides developers with the capability to send notifications to their applications. This key enables sending real-time notifications to the desired application using the Firebase Cloud Messaging (FCM) service. By using the Firebase Server Key, you can add customized, targeted, and interactive notifications to your application. This allows you to convey important updates to your users, enhance user engagement through interaction, and improve the overall user experience. Thanks to Firebase&apos;s reliable infrastructure, your application notifications reach the target audience quickly and reliably, providing users with a better communication experience.
